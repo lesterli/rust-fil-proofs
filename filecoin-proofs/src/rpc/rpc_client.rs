303200926fd926e61bb0aa48c119233e962c73d6
lesterli <lester123@live.cn>
2020-02-14 17:08:23 +0800

    update rpc client

--- filecoin-proofs/src/rpc/rpc_client.rs
+++ filecoin-proofs/src/rpc/rpc_client.rs
@@ -1,4 +1,5 @@
-use jsonrpc_core_client::{RpcChannel, TypedClient};
+use jsonrpc_core::{Error, ErrorCode, IoHandler, Params, Value};
+use jsonrpc_http_server::*;
 
 #[derive(Clone)]
 struct RpcClient(TypedClient);
@@ -6,5 +7,17 @@ struct RpcClient(TypedClient);
 impl From<RpcChannel> for RpcClient {
     fn from(channel: RpcChannel) -> Self {
         RpcClient(channel.into())
+    }
+}
+
+impl RpcClient {
+    fn hello(&self, msg: &'static str) -> impl Future<Item = String, Error = RpcError> {
+        self.0.call_method("hello", "String", (msg,))
+    }
+    fn fail(&self) -> impl Future<Item = (), Error = RpcError> {
+        self.0.call_method("fail", "()", ())
+    }
+    fn notify(&self, value: u64) -> impl Future<Item = (), Error = RpcError> {
+        self.0.notify("notify", (value,))
     }
 }
